106dbcd05db28f980ec4e5f8f5b73ca69bb07324
Dave Neeley <dave.neeley@gmail.com>
2023-04-15 20:01:22 -0600

    mark file executable

--- lib/commands/command-nerdify.bash
+++ lib/commands/command-nerdify.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
-binDir="${ASDF_INSTALL_PATH}/bin"
+# file is marked executable so that it is executed directly instead of asdf sourcing it
+binDir="${ASDF_INSTALL_PATH}"/bin
 export CNI_PATH=$binDir/cni
 echo $CNI_PATH
+echo $ASDF_CMD_FILE
